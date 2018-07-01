ea25ac492547fbf9777de4d23ce38b3c22b70994
Branislav Bugarcic <bugarcic@gmail.com>
2018-07-01 12:02:35 +0200

    configure cors for development and production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins Rails.env.production? ? 'https://cash-machine-react-frontend.herokuapp.com/withdrawal' : '*'
 
     resource '*',
       headers: :any, 
